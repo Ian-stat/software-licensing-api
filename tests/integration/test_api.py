76d95cb1e4bcce7517acadd29b1703e1b833f723
rara64 <60274052+rara64@users.noreply.github.com>
2026-01-19 16:47:39 +0100

    Fix build

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,12 +1,13 @@
 import pytest
-import requests
 from app import app
 
 
 @pytest.fixture
-def app():
-    yield app
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
     
 def test_health():
-    response = requests.get("/")
+    response = client.get("/")
     assert response.status_code == 200
