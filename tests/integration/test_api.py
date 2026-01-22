3c3fbcc62233eef3ee6626874102df87b1050bbf
rara64 <60274052+rara64@users.noreply.github.com>
2026-01-22 15:25:23 +0100

    Working API test

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,13 +1,11 @@
 import pytest
 from app import app
 
-
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
-    
-def test_health():
+    return app.test_client()
+
+# demko
+def test_health(client):
     response = client.get("/")
     assert response.status_code == 200
